37601c18c198745021e3fd57f96f1b2bf36efc82
Dominique BILLET <d.billet@sied70.fr>
2022-03-18 11:30:07 +0100

    First commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="blue">Must be blue blacground</div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.blue {
+  background-color: $blue-6;
+}
+</style>
